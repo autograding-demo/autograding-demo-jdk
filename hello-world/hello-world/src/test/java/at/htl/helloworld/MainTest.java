d886408c6c2e613e3ea42e951c4aaa8abc46ae78
stuetz <t.stuetz@htl-leonding.ac.at>
2022-05-06 21:00:01 +0200

    add main-method w/ test

---
+++ hello-world/hello-world/src/test/java/at/htl/helloworld/MainTest.java
@@ -0,0 +1,51 @@
+package at.htl.helloworld;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class MainTest {
+
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @BeforeEach
+    public void setupOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @AfterEach
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+
+    @Test
+    void should_printHelloworld() {
+        Main.main(null);
+        assertThat(getOutput()).isEqualTo("Hello World!\n");
+    }
+}
